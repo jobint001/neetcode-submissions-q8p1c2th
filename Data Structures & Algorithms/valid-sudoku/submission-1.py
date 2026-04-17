be3dcf1f8971edf08465c5526a46aa3d7fd0f7d5
Jobin Tom <92356462+jobint001@users.noreply.github.com>
2026-04-18 00:34:05 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,45 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        tempSet = set()
+
+        for i in range(0,9):
+            tempSet=set()
+            for j in range(0,9):
+                if board[i][j] == '.':
+                    continue
+                if board[i][j] in tempSet:
+                    print("1")
+                    return False
+                else:
+                    tempSet.add(board[i][j])
+
+        for i in range(0,9):
+            tempSet=set()
+            for j in range(0,9):
+                if board[j][i] == '.':
+                    continue
+                if board[j][i] in tempSet:
+                    print("2")
+                    return False
+                else:
+                    tempSet.add(board[j][i])
+        j,k=0,0
+        for i in range(0, 9):
+            tempSet = set()
+            for row in range(j,j+3):
+                for col in range(k,k+3):
+                   
+                    if board[row][col] == ".":
+                        continue
+                    if board[row][col] in tempSet:
+    
+                        return False
+                    else:
+                        tempSet.add(board[row][col])
+            if i in(0,1,3,4,6,7):
+                k+=3
+            else:
+                k=0
+                j+=3
+        
+        return True
